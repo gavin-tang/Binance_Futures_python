376e2cdbc5d2eb9a2b066d27868ecb78816beed1
tangwei <gavin.tangwei@gmail.com>
2021-08-29 16:31:36 +0800

    init

--- binance_f/constant/test.py
+++ binance_f/constant/test.py
@@ -4,8 +4,8 @@ if(os.path.exists("binance_f/privateconfig.py")):
     g_api_key = p_api_key
     g_secret_key = p_secret_key
 else:
-    g_api_key = ""
-    g_secret_key = ""
+    g_api_key = "FrzEVVrMO55G9lV461jPheMeHgjEQoNuzuLEz7llQuV41r7y8VXYhiuI3wWhZlFT"
+    g_secret_key = "3qwytN7WWLoshdnklikdhRdrE1Ckbu4CV9CTnwOZPWQ2q9Wz8D9rAyR5mJ89K0P3"
 
 
 g_account_id = 12345678
